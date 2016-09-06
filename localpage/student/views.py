6191c13167b5ce5d47c1fc687c071f3aa6a70b8c
frank ramos <ramosfp99@gmail.com>
2016-09-05 23:59:26 -0700

    add urls and views

--- localpage/student/views.py
+++ localpage/student/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from student.models import SchoolYear, Semister, Module, Instructor, Block, Student, Subject, SubjectSchedule
 # Create your views here.
+def studentIndex(request):
+	student=Student.objects.all()
+	context={
+		"title": 'Student list',
+		"student_list": student,
+	}
+	return render(request, "student/student_list.html", context)
