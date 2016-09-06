8fc4ed54b4795f8772f78b57e85c8aba32050972
frank ramos <ramosfp99@gmail.com>
2016-09-05 23:51:07 -0700

    add model to admin

--- localpage/student/admin.py
+++ localpage/student/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from student.models import SchoolYear, Semister, Module, Instructor, Block, Student, Subject, SubjectSchedule
 # Register your models here.
+
+
+
+
+
+admin.site.register(SchoolYear)
+admin.site.register(Semister)
+admin.site.register(Module)
+admin.site.register(Instructor)
+admin.site.register(Block)
+admin.site.register(Student)
+admin.site.register(Subject)
+admin.site.register(SubjectSchedule)
